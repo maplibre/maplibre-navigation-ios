83cc0864f85d11619373d298e49650dc2fb35fdb
Maarten Zonneveld <maartenzonneveld@live.nl>
2024-02-16 09:49:02 +0100

    Revert let voiceController change

--- XcodeProj/Examples/Swift/CustomViewController.swift
+++ XcodeProj/Examples/Swift/CustomViewController.swift
@@ -17,7 +17,7 @@ class CustomViewController: UIViewController, MLNMapViewDelegate {
     var userRoute: Route?
 
     // Start voice instructions
-//    let voiceController = MapboxVoiceController()
+    let voiceController = RouteVoiceController()
     
     var stepsViewController: StepsViewController?
 
