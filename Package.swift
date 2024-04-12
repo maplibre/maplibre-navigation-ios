d73147875e69e6e874f35db4ab60d672e646ef7e
Patrick Kladek <patrick.kladek@outlook.com>
2024-04-12 19:14:41 +0400

    remove macOS platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "maplibre-navigation-ios",
     defaultLocalization: "en",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "MapboxNavigation",
