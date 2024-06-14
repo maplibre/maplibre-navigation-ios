09c8fe962db6d8910981b4c370029c48e47a0de4
Michael Kirk <michael.code@endoftheworl.de>
2024-06-13 19:36:58 -0700

    fix and re-run extract_localizable.sh

--- scripts/extract_localizable.sh
+++ scripts/extract_localizable.sh
@@ -9,19 +9,17 @@ LANGUAGES=( "Base" )
 
 for lang in ${LANGUAGES[@]}
 do
+    source "${DIR}/file_conversion.sh"
+
     echo "Extracting ${lang} strings"
 
     # Extract localizable strings from .swift files
     find ${NAVIGATION} -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o "${NAVIGATION}/Resources/${lang}.lproj"
     STRINGS_FILE="${NAVIGATION}/Resources/${lang}.lproj/Localizable.strings"
+    convertIfNeeded "$STRINGS_FILE"
 
     # Extract localizable strings from .swift files
     find ${CORE} -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o "${CORE}/Resources/${lang}.lproj"
     STRINGS_FILE="${CORE}/Resources/${lang}.lproj/Localizable.strings"
-
-    source "${DIR}/file_conversion.sh"
-
-    # Convert UTF-16LE generated files to UTF-8
-    iconv -f UTF-16LE -t UTF-8 ${STRINGS_FILE} > ${STRINGS_FILE}.new
-    mv -f ${STRINGS_FILE}.new ${STRINGS_FILE}
+    convertIfNeeded "$STRINGS_FILE"
 done
