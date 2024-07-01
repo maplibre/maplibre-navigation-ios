b0c1397a7e462476b2fb2e22895545493a041601
Patrick Kladek <patrick.kladek@outlook.com>
2024-07-01 16:29:31 +0200

    prevent code from running again after setting the same property again

--- MapboxNavigation/NavigationViewController.swift
+++ MapboxNavigation/NavigationViewController.swift
@@ -214,6 +214,8 @@ open class NavigationViewController: UIViewController {
 	
     var currentStatusBarStyle: UIStatusBarStyle = .default {
         didSet {
+            guard oldValue != self.currentStatusBarStyle else { return }
+
             self.mapViewController.instructionsBannerView.backgroundColor = InstructionsBannerView.appearance().backgroundColor
             self.mapViewController.instructionsBannerContentView.backgroundColor = InstructionsBannerContentView.appearance().backgroundColor
         }
@@ -228,6 +230,8 @@ open class NavigationViewController: UIViewController {
      */
     public var route: Route? {
         didSet {
+            guard oldValue != self.route else { return }
+			
             if let route {
                 if self.routeController == nil {
                     let routeController = RouteController(along: route, directions: self.directions, locationManager: self.locationManager)
@@ -283,6 +287,8 @@ open class NavigationViewController: UIViewController {
      */
     public var routeController: RouteController? {
         didSet {
+            guard oldValue != self.routeController else { return }
+			
             self.mapViewController.routeController = self.routeController
         }
     }
@@ -313,6 +319,8 @@ open class NavigationViewController: UIViewController {
      */
     public var automaticallyAdjustsStyleForTimeOfDay = true {
         didSet {
+            guard oldValue != self.automaticallyAdjustsStyleForTimeOfDay else { return }
+			
             self.styleManager.automaticallyAdjustsStyleForTimeOfDay = self.automaticallyAdjustsStyleForTimeOfDay
         }
     }
