00880464be0c31cf0a952008a8b8d9abc501541d
Michael Kirk <michael.code@endoftheworl.de>
2024-07-29 15:36:06 -0700

    use latest turf

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/flitsmeister/mapbox-directions-swift", exact: "0.23.3"),
-        .package(url: "https://github.com/flitsmeister/turf-swift", exact: "0.2.2"),
+        .package(url: "https://github.com/mapbox/turf-swift.git", from: "2.8.0"),
         .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution.git", from: "6.0.0"),
         .package(url: "https://github.com/ceeK/Solar.git", exact: "3.0.1"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.53.6")
