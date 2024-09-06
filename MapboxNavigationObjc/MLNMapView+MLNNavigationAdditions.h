22b3107c8be2bdf674d39f463e4162c5a6230640
Michael Kirk <michael.code@endoftheworl.de>
2024-09-06 13:59:44 -0700

    Cleanup: remove private header (#95)
    This header was copied in from MapLibre Native to expose a private
    method.
    
    Previously this got out of sync and caused a bug:
    https://github.com/maplibre/maplibre-navigation-ios/pull/53
    
    Instead we can use the already public delegate method.
    
    Note: The deleted header was importing Maplibre/Mapbox.h, but we are
    using that elsewhere, so I moved that import up a level.

--- MapboxNavigationObjc/MLNMapView+MLNNavigationAdditions.h
+++
@@ -1,9 +0,0 @@
-#import <MapLibre/Mapbox.h>
-
-@interface MLNMapView (MLNNavigationAdditions)
-
-- (void)mapViewDidFinishRenderingFrameFullyRendered:(BOOL)fullyRendered
-                                  frameEncodingTime:(double)frameEncodingTime
-                                 frameRenderingTime:(double)frameRenderingTime;
-
-@end
